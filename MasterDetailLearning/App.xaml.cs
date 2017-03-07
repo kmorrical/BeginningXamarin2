48fd07401d83db10eaaab33740fed79826d447cb
kmorrical <kathryn.morrical@gmail.com>
2017-03-07 15:29:45 -0800

    first commit

---
+++ MasterDetailLearning/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace MasterDetailLearning
+{
+	public partial class App : Application
+	//inherits from application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MasterDetailLearningPage();
+			//tells the application what the root page is
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ MasterDetailLearning/MasterDetailLearningPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace MasterDetailLearning
+{
+	public partial class MasterDetailLearningPage : MasterDetailPage
+	{
+		public MasterDetailLearningPage()
+		{
+			InitializeComponent();
+			Detail = new NavigationPage(new Page1());
+			IsPresented = false;
+		}
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			Detail = new NavigationPage(new Page1());
+			IsPresented = false;
+		}
+		void Handle_Clicked2(object sender, System.EventArgs e)
+		{
+			Detail = new NavigationPage(new Page2());
+			IsPresented = false;
+		}
+	}
+}
